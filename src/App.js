c35a03fcfbfbedf69152a462ada0f8cc33a69f2c
Eiden0 <shibashis12@gmail.com>
2019-12-31 04:43:07 +0530

    added preloader
    BTW i installed packages with npm so there is an extra file called package.json for that
    **IMPORTANT**
    I installed a new library called react-preloader"

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Notifications from './Components/Notifications'
 import Blog from './Components/Blog'
 import Reporter from './Components/Reporter'
 import {BrowserRouter, Route} from 'react-router-dom';
+import {Lines,Cube,Sugar} from 'react-preloaders'
 
 
 class App extends Component{
@@ -16,19 +17,22 @@ class App extends Component{
 	
 	render(){
 			return(
-				<BrowserRouter>
-					<div>
-						<Navbar />
-						<Header />
-						<Route exact path="/" component={Home_container} />
-						<Route path="/Notifications" component={Notifications} />
-						<Route path="/Units" component={Units} />
-						<Route path="/Team" component={Team} />
-						<Route path="/Blog" component={Blog} />
-                                                <Route path="/Submit" component={Reporter} />
-						<Footer />		    		
-					</div>
-				</BrowserRouter>
+				<div>
+					<BrowserRouter>
+						<div>
+							<Navbar />
+							<Header />
+							<Route exact path="/" component={Home_container} />
+							<Route path="/Notifications" component={Notifications} />
+							<Route path="/Units" component={Units} />
+							<Route path="/Team" component={Team} />
+							<Route path="/Blog" component={Blog} />
+													<Route path="/Submit" component={Reporter} />
+							<Footer />
+						</div>
+					</BrowserRouter>
+				<Sugar background="#202020" color="white" time="3000"/>
+				</div>
 			)
 	}
 }
